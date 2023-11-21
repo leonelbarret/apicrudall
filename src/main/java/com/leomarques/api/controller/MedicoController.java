675a5528705caca7675f9e5944df2a75b612e09d
Leonel Barreto <leonelbarret@gmail.com>
2023-11-21 20:54:50 -0300

    Adicionando as validacões

--- src/main/java/com/leomarques/api/controller/MedicoController.java
+++ src/main/java/com/leomarques/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package com.leomarques.api.controller;
 import com.leomarques.api.medico.DadosCadastroMedico;
 import com.leomarques.api.medico.Medico;
 import com.leomarques.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repo.save(new Medico(dados));
 
     }
